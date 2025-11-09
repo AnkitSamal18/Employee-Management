ac8db8a4fac25d1f922d0506d058a97918dabc4c
niharikapradhan <niharikapradhan@localhost>
2025-11-09 19:11:42 +0530

    update in login and registration servlet code

--- src/main/java/ctrls/RegisterServlet.java
+++ src/main/java/ctrls/RegisterServlet.java
@@ -29,7 +29,7 @@ public class RegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubz
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 	
